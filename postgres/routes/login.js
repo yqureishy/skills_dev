c8cfa20e47c8bf23cf121980932ef2d0236a64e0
yqureishy <yqureishy1993@gmail.com>
2021-06-23 17:55:24 -0500

    created routs for login and register

---
+++ postgres/routes/login.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('login')
+})
+
+router.post('/', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    db.one('SELECT * from users WHERE username=$1', [username])
+        .then((user) => {
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (result == true) {
+                    if (req.session) {
+                        req.session.userID = user.user_id
+                        req.session.username = user.username
+
+                        res.redirect('/profile')
+                    }
+                }
+                else {
+                    res.render('login', { message: 'Incorrect Username or Password' })
+                }
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+            res.send('Incorrect username/password. Please try again.')
+        })
+})
+
+
+
+
+module.exports = router
---
+++ postgres/routes/register.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/register', (req, res) => {
+    res.render('register')
+})
+
+router.post('/register', (req, res) => {
+    const username = req.body.username
+    const password = req.body.password
+
+    bcrypt.genSalt(10, function (error, salt) {
+        bcrypt.hash(password, salt, function (error, hash) {
+            if (!error) {
+                db.none('INSERT INTO users (username, password) VALUES ($1,$2)', [username, hash])
+                    .then(() => {
+                        res.redirect('/login')
+                    })
+                    .catch((err) => {
+                        console.log(err)
+                    })
+            }
+            else {
+                console.log(error)
+            }
+        })
+    })
+})
+
+module.exports = router

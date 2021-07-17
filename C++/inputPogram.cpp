895aa2289a11c153b9783da8c93d5a0b0d4f2bb2
yqureishy <yqureishy1993@gmail.com>
2021-07-17 16:23:48 -0500

    installed and setup c++

---
+++ C++/inputPogram.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    string first_name, last_name;
+    cout<<"Enter your first name: ";
+    cin>>first_name;
+
+    cout<<"Enter your last name: ";
+    cin>>last_name;
+
+    cout<<"Your full name is: "<<first_name<<" "<<last_name<<endl;
+
+    return 0;
+}
---
+++ C++/testing.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+cout<<"Testings code now again!!";
+
+return 0;
+
+}

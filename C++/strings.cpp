e2e7ae8112da555acf678d67da85e37446b5a501
yqureishy <yqureishy1993@gmail.com>
2021-07-17 18:51:37 -0500

    learned find and substring methods on string

---
+++ C++/strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+string name = "Younus Qureishy";
+
+cout<<name[0]<<endl;
+cout<<name.find("un")<<endl;
+cout<<name.substr(7,5)<<endl;
+cout<<name;
+
+
+    return 0;
+}

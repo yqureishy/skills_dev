6cedbf617aac6898d75d28afbd6afa83c4e69a05
yqureishy <yqureishy1993@gmail.com>
2021-07-17 19:06:19 -0500

    worked with math functions and floats/double

---
+++ C++/numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int wholeNumber = 10;
+    double decimalNumber = 2.4;
+    float decimalNumber2 = 3;
+
+    cout<<wholeNumber/decimalNumber2<<endl;
+    cout<< 4+3.2 <<endl;
+    cout<<pow(2,5)<<endl;
+    cout<<sqrt(49.24)<<endl;
+
+
+    return 0;
+}

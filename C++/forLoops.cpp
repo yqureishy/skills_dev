894ccc6a8a4ee03d04010f54309c2a5e1cf1954b
yqureishy <yqureishy1993@gmail.com>
2021-07-22 16:14:25 -0500

    learned for loops

---
+++ C++/forLoops.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+// int index = 0;
+// while(index < 5){
+//     cout<< index<<endl;
+//     index++;
+// }
+
+int myArray[] = {3, 7, 11, 13, 17};
+string names[] = {"Younus", "Manal", "Abdullah", "Suleman", "Hakim"};
+
+for(int i = 0; i <= 4; i++){
+    cout << myArray[i] << endl;
+    cout << names[i] << endl;
+}
+
+    return 0;
+}
---
+++ C++/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+int index = 0;
+// while(index < 5){
+//     cout<< index<<endl;
+//     index++;
+// }

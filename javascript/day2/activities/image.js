63c5d3c99f7f6221ef5520ef0f6c9c7ea853ef57
yqureishy <yqureishy1993@gmail.com>
2021-08-09 13:35:33 -0500

    added image generator

---
+++ javascript/day2/activities/image.js
@@ -0,0 +1,18 @@
+var uoList = document.getElementById('uoList')
+var btnEnter = document.getElementById("submit")
+var textboxImageURL = document.getElementById("textboxImageURL")
+
+btnEnter.addEventListener('click', () => {
+    // URL stored in imageURL //
+    let imageURL = textboxImageURL.value
+    // created <img>
+    let posterImage = document.createElement('img')
+    // added URL to source attribute //
+    posterImage.setAttribute("src", imageURL)
+    // appended image to UOLIST //
+    uoList.appendChild(posterImage)
+
+
+
+
+})

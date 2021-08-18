914c21ea4a99a7d8c62158ab616dc623550932f0
yqureishy <yqureishy1993@gmail.com>
2021-08-18 17:11:52 -0500

    created HOC header

--- React/activities/src/App.js
+++ React/activities/src/App.js
@@ -1,14 +1,15 @@
 import Name from './Components/Name'
 import Friends from './Components/Friends'
+import Header from './Components/Header'
 
 function App() {
-  let names = ["Manal", "Masood", "Mikael", "Maisum"]
+  // let names = ["Manal", "Masood", "Mikael", "Maisum"]
   return (
     <div className="App">
-      <p>Name</p>
+      {/* <p>Name</p>
       <Name firstName="Younus" lastName="Qureishy" />
-      <Friends allFriends={names} />
-
+      <Friends allFriends={names} /> */}
+      <Header />
     </div>
   );
 }

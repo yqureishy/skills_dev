ef805c703552a07ad4b1d72f38cfcdc35f889fab
yqureishy <yqureishy1993@gmail.com>
2021-11-27 19:09:54 -0600

    input flow working

--- javascript/DollarsBankApp/app.js
+++ javascript/DollarsBankApp/app.js
@@ -1,9 +1,35 @@
 
 import ps from "prompt-sync";
-const prompt = ps();
+const input = ps({ sigint: true });
 
-import { helloWorld, mainMenu } from "./src/functions.js";
+import { mainMenu } from "./src/functions.js";
 
-mainMenu()
-let choice = prompt("Enter Choice (1,2 or 3): ");
+
+
+let choice = 0;
+
+while (choice != 3) {
+    try {
+
+        mainMenu()
+        choice = input("Enter Choice (1,2 or 3): ");
+
+        switch (Number(choice)) {
+            case 1: console.log("You chose 1!");
+                break;
+            case 2: console.log("You chose 2!");
+                break;
+            case 3: console.log("You chose 3!");
+                break;
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+    console.log("\n\n");
+
+};
+
 

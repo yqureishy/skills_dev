8c520dec01f448ef4bddb82193c3ebbf22c67b24
yqureishy <yqureishy1993@gmail.com>
2021-12-02 14:28:38 -0600

    menu details created

--- javascript/DollarsBankApp/app.js
+++ javascript/DollarsBankApp/app.js
@@ -1,12 +1,12 @@
 
 import ps from "prompt-sync";
-import Account from "./src/account.js"
+// import { Account } from "./src/account.js"
 // const Account = require("./account.js")
 const input = ps({ sigint: true });
 
 import { mainMenu, newUser, printAccounts, login } from "./src/functions.js";
 
-
+let x = "/^\$ ? ([0 - 9]{ 1, 3}, ([0 - 9]{ 3},) * [0 - 9]{ 3 }| [0 - 9] +) (.[0 - 9][0 - 9]) ? $/"
 
 let choice = 0;
 
@@ -17,8 +17,7 @@ while (choice != 3) {
         choice = input("Enter Choice (1,2 or 3): ");
 
         switch (Number(choice)) {
-            case 1: console.log("You chose 1!");
-                newUser()
+            case 1: newUser()
                 printAccounts()
                 break;
             case 2: login()

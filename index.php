4729ef4ebd92a32ace6d56591e2367f48a1523f6
yqureishy <78046658+yqureishy@users.noreply.github.com>
2022-01-06 12:48:52 -0600

    Add files via upload
    Adding php tutorial

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+
+<?php
+
+echo "Hello World!!";
+echo "<br>";
+
+$loggedIn = false;
+
+if($loggedIn == true){
+    echo "You are logged in!";
+} else {
+    echo "You are not logged in!";
+}
+
+echo "I'm Younus";
+echo 'I\'m Younus';
+
+?>
+    
+</body>
+</html>
